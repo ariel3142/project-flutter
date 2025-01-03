d4967150e5bc45b85a9904d7d40e9de11c25aad8
Ariel Putra <158743626+ariel3142@users.noreply.github.com>
2025-01-03 14:01:20 +0700

    Update main.dart

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -1,19 +1,79 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'login_page.dart';
-
+import 'package:flutter_application_1/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    )
+  );
 }
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'UAS Mobile',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: LoginPage(),
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  FadeInUp(duration: const Duration(milliseconds: 1000), child:const Text("Selamat Datang", style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30
+                  ),)),
+                  const SizedBox(height: 20,),
+                  FadeInUp(duration: const Duration(milliseconds: 1200), child: Text("Hasil Project UAS Mobile Programming", 
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15
+                  ),)),
+                ],
+              ),
+              FadeInUp(duration: const Duration(milliseconds: 1400), child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/illustration.png')
+                  )
+                ),
+              )),
+              Column(
+                children: <Widget>[
+                  FadeInUp(duration: const Duration(milliseconds: 1500), child: MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(
+                        color: Colors.black
+                      ),
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: const Text("P Login", style: TextStyle(
+                      fontWeight: FontWeight.w600, 
+                      fontSize: 18
+                    ),),
+                  )),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
